a0cf18b49106d6d893516c8adcc67f8a4704a369
ozgur <ozgur_1694@hotmail.com>
2020-10-31 23:05:36 +0300

    react pass lists

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import GoalList from './Component/GoalList'
-
 const App = () => {
+
+    const courseGoal = [
+        {id: 'cg1', text: 'watch'},
+        {id: 'cg2', text: 'apply'},
+        {id: 'cg3', text: 'push'},
+        {id: 'cg4', text: 'new var.'}
+        ];
+
     return(
         <div className="course-goals">
             <h2 >second app</h2>
-            <GoalList/>
+            <GoalList goals={courseGoal}/>
+            }
         </div>
         // return React.createElement('h1', {title: 'its works'}, 'hi first app!')
     );
--- src/Component/GoalList.js
+++ src/Component/GoalList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const GoalList = () => {
+const GoalList = props => {
     return (
         <ul className="goal-list">
-            <li>watch</li>
-            <li>apply</li>
-            <li>push</li>
-            <li>from new class</li>
+            {props.goals.map((goal) => {
+                return <li key={goal.id}>{goal.text}</li>
+            })
+            }
         </ul>
     );
 };
