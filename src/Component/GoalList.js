ed5183186fc47020e2e4aca238e24028361adbe7
ozgur <ozgur_1694@hotmail.com>
2020-10-31 17:33:29 +0300

    add new class

---
+++ src/Component/GoalList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const GoalList = () => {
+    return (
+        <ul className="goal-list">
+            <li>watch</li>
+            <li>apply</li>
+            <li>push</li>
+            <li>from new class</li>
+        </ul>
+    );
+};
+
+export default GoalList;
