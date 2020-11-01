3e55f9a2cf5777560919e5f5322ee08ead17eb77
ozgur <ozgur_1694@hotmail.com>
2020-11-01 19:38:49 +0300

    state

--- src/Component/NewGoal/NewGoal.js
+++ src/Component/NewGoal/NewGoal.js
@@ -3,21 +3,26 @@ import React from 'react';
 import './NewGoal.css';
 
 const NewGaol = props => {
+    let userText = '';
     const addGoalHandler = event => {
         event.preventDefault();
 
         const newGoal = {
             id : Math.random().toString(),
-            text: 'its new goal'
+            //text: 'its new goal'
+            text: userText
         };
-
         props.onAddGoal(newGoal);
 
     };
 
+    const textChangeHandler = event => {
+        userText = event.target.value;
+    }
+
     return (
         <form className="new-goal" onSubmit={addGoalHandler}>
-        <input type="text"/>
+        <input type="text" onChange={textChangeHandler}/>
         <button type="submit">Add Goal</button>
     </form>
 
